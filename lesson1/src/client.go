b80f57344d51800895721e3d3e2b92627b86c589
tsthght <tsthght@163.com>
2022-05-12 21:15:21 +0800

    add lesson1

---
+++ lesson1/src/client.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"context"
+	"google.golang.org/grpc"
+	"log"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:1234", grpc.WithInsecure())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	client := NewHelloServiceClient(conn)
+	reply, err := client.Hello(context.Background(), &String{Value: "hello"})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(reply.GetValue())
+}
